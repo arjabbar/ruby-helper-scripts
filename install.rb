619e6341ff13578c6b2f1680fd2d040a205bdc04
Abdur Jabbar <arjabbar@yahoo.com>
2014-11-22 16:54:48 -0600

    Added script for creating helper scripts.

--- install.rb
+++ install.rb
@@ -20,14 +20,36 @@ end
 def scripts_without_hashbangs
   scripts.select do |script_file|
     File.readlines(script_file)
-      .select{|line| line.include? '#!'}
+      .select{|line| line.start_with? '#!'}
       .empty?
   end
 end
 
+def using_zsh?
+  ENV['SHELL'].include? 'zsh'
+end
+
+def shell_rc_file
+  return '~/.zshrc' if using_zsh?
+  '~/.bashrc'
+end
+
+current_user = ENV['USER']
+
 scripts_without_hashbangs
 
 scripts_without_hashbangs.each do |file|
+  puts "Adding hash bang to #{file}".green
   lines = File.readlines file
-  File.writelines(file, [RubyInfo.hash_bang] + lines)
+  new_file_content = (["#{RubyInfo.hash_bang}\n"] + lines).join
+  File.write file, new_file_content
+end
+
+scripts.each do |script|
+  script_link_path = "/usr/local/bin/#{script.split('/').last}"
+  system "sudo chmod +x #{script}"
+  system "sudo chmod o-w #{script}"
+  system "sudo ln -f #{File.join Dir.pwd, script} #{script_link_path}"
+  system "sudo chown #{current_user}: #{script_link_path}"
+  system "$SHELL #{shell_rc_file}"
 end
